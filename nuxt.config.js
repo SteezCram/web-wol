ab92f618179bfde3da4281c1b9dec7bfba521f60
Steez <t.croizet77@gmail.com>
2021-12-02 19:24:24 +0100

    [FIX] Add x.css in the nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,7 +55,8 @@ export default
 
     // Global CSS: https://go.nuxtjs.dev/config-css
     css: [
-        '~/assets/css/icons.css'
+        '~/assets/css/icons.css',
+        '~/assets/css/x.css',
     ],
 
     // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
